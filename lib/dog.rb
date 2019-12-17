f071584f47720e82253adcadeeee18180180df25
Kate Summerill <kaitlynsummerill@gmail.com>
2019-12-17 23:49:29 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,10 @@
 # Your code goes here!
 class Dog
-
+  # write/setter method
   def name=(dog_name)
     @this_dogs_name = dog_name
   end
-
+  # read/getter method
   def name
     @this_dogs_name
   end
