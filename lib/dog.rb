8461a3689300c553a2e4d1105ab04c4c1441631b
Kate Summerill <kaitlynsummerill@gmail.com>
2019-12-17 23:47:44 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,12 @@
 # Your code goes here!
 class Dog
 
-  def name=(dog_name)
-    @this_dogs_name = dog_name
+  def name=(name)
+    @this_dogs_name = name
   end
 
   def bark
     puts "woof!"
   end
-  
+
 end
